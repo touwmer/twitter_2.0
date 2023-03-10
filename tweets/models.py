0a45e2252f07066e3c5601a3ceba2a04073bfb33
touwmer <yanliu.hust.edu@gmail.com>
2023-03-10 00:29:39 -0600

    tweet api and unit tests

--- tweets/models.py
+++ tweets/models.py
@@ -11,6 +11,9 @@ class Tweet(models.Model):
     )
     content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        index_together = (('user', 'created_at'),)
+        ordering = ('user', '-created_at')
 
     @property
     def hours_to_now(self):
