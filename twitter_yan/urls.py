520f82e3c5d47482687f43ae0e333a229c0cc050
touwmer <yanliu.hust.edu@gmail.com>
2023-04-07 23:45:13 -0500

    create likes

--- twitter_yan/urls.py
+++ twitter_yan/urls.py
@@ -21,12 +21,14 @@ from tweets.api.views import TweetViewSet
 from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
 from comments.api.views import CommentViewSet
+from likes.api.views import LikeViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', UserViewSet)
 router.register(r'api/accounts', AccountViewSet, basename='accounts')
 router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
+router.register(r'api/likes', LikeViewSet, basename='likes')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 router.register(r'api/comments', CommentViewSet, basename='comments')
 
